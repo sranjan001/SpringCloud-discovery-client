b2d13319404fa85c6f5e60c322c1082421786684
sranjan001 <sranjan@pivotal.io>
2020-01-12 00:06:32 +0800

    commit

---
+++ src/main/java/com/satya/SpringCloudeurekaclient/ClientController.java
@@ -0,0 +1,32 @@
+package com.satya.SpringCloudeurekaclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("get-greet")
+    public String getGreeting() {
+        return restTemplate.getForEntity("//EUREKA-SERVER-TEST/greet", String.class).getBody();
+    }
+
+    @GetMapping("/services")
+    public List<ServiceInstance> getDiscoveryClients() {
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("EUREKA-SERVER-TEST");
+
+        return serviceInstanceList;
+    }
+}
